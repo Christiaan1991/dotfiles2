ed5b9c4496a142e1e6b1597b22433de2b8cf8279
Christiaan Post <lcpost91@gmail.com>
2024-05-17 23:11:49 +0200

    add dotfiles git command to fish, extra comments

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,15 +3,27 @@ set -x TERM xterm-color
 set -x GO111MODULE on
 set -x AWS_REGION us-east-1
 
+# docker commands
 alias dup="docker compose up -d"
 alias down="docker compose down"
 alias dops="docker ps"
 alias dil="docker kill"
 alias dip="docker image prune -a --filter " # followed by format: "until=2024-01-01T00:00:00"
 alias dsp="docker system prune"
+
+# golang test directory
 alias got="go test ./..."
+
+# create git tag
 alias gt="git tag -l | sort -V | tail"
+
+# yubikey shortcut
 alias y="ykman oath accounts code --single | xclip -selection clip -rmlastnl"
+
+# neovim shortcut
 alias n="nvim ."
+
+# add git command for dotfiles
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
 thefuck --alias | source
